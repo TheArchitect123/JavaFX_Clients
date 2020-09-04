49d26a061ac51f5b86afdbcebde15af2d0f8e267
Dan Gerchcovich <dan.developer789@gmail.com>
2020-09-04 10:33:16 -0700

    Setting up OpenWeather app

---
+++ open_weather_connect/src/main/java/client/ApplicationMngr.java
@@ -0,0 +1,30 @@
+package client;
+
+import javafx.application.*;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class ApplicationMngr extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+//        btn.setOnAction(new EventHandler<ActionEvent>() {
+// 
+//            @Override
+//            public void handle(ActionEvent event) {
+//                System.out.println("Hello World!");
+//            }
+//        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+	}
+}
---
+++ open_weather_connect/src/main/java/client/Main.java
@@ -0,0 +1,11 @@
+package client;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("MMAHAHAA");
+	    ApplicationMngr.launch(args);
+	}
+}
